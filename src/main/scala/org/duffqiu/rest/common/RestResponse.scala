5082242158b205facfe8e583bf098d3f201e5616
egecqiu <duff.gechuan.qiu@ericsson.com>
2014-06-16 09:29:54 +0800

    style fix

--- src/main/scala/org/duffqiu/rest/common/RestResponse.scala
+++ src/main/scala/org/duffqiu/rest/common/RestResponse.scala
@@ -3,25 +3,25 @@ package org.duffqiu.rest.common
 import org.duffqiu.rest.common.RestUtility.asJson
 
 object RestResponse {
-    private final val DEFAULT_STATUS_CODE = 200
+	private final val DEFAULT_STATUS_CODE = 200
 }
 
 case class RestResponse(statusCode: Int = RestResponse.DEFAULT_STATUS_CODE, headerPara: RestParameters = RestParameters("Header"), body: AnyRef = EmptyBody) {
-    val bodyJson: String = body match {
-        case s: String => s
-        case _ => asJson(body)
-    }
+	val bodyJson: String = body match {
+		case s: String => s
+		case _ => asJson(body)
+	}
 
-    //get header parameter's value
-    def >:>(key: String) = headerPara(key)
+	//get header parameter's value
+	def >:>(key: String) = headerPara(key)
 
-    //append header parameter value
-    def <:<(t: (String, String)) = new RestResponse(statusCode, headerPara + (t._1 -> t._2), body)
+	//append header parameter value
+	def <:<(req: String, resp: String) = new RestResponse(statusCode, headerPara + (req -> resp), body)
 
-    //append body
-    def <<<(newBody: AnyRef) = new RestResponse(statusCode, headerPara, newBody)
+	//append body
+	def <<<(newBody: AnyRef) = new RestResponse(statusCode, headerPara, newBody)
 
-    //apend header map
-    def <:<(map: Map[String, String]) = new RestResponse(statusCode, headerPara ++ map, body)
+	//apend header map
+	def <:<(map: Map[String, String]) = new RestResponse(statusCode, headerPara ++ map, body)
 
 }
